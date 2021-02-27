53dc539b131fac9b6e85456f831e46f46e93ce44
BOOTON24hr <79717481+BOOTON24hr@users.noreply.github.com>
2021-02-27 14:57:47 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+pkg upgrade && pkg update
+pkg install git
+pkg install tesseract
+git clone https://github.com/BOOTON24hr/BOOTON24por48
+cd  BOOTON24por48
+pkg install ffmpeg 
+pkg install wget
+pkg install nodejs
+pkg install npm
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i -g ytdl
+npm i
+npm i got
+node index.js
